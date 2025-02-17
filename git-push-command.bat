662afc366078065d28dc225cc0e0d8c1040639da
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-02-17 16:14:14 +0000

    Changed the script to .y

--- git-push-command.bat
+++ git-push-command.bat
@@ -3,7 +3,7 @@ REM Activate the virtual environment
 call "C:\Users\SynergexSystems\Documents\utilities\github-push-script\.venv\Scripts\activate.bat"
 
 REM Run the Python script
-python "C:\Users\SynergexSystems\Documents\utilities\github-push-script\git-push-command.bat"
+python "C:\Users\SynergexSystems\Documents\utilities\github-push-script\git-push-command.py"
 
 REM Deactivate the virtual environment
 deactivate
