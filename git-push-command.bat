a8fd9c1b2a77bce89268eb9615a1aec51e4acec4
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-02-17 11:33:40 +0000

    inital commit of .BAT file

---
+++ git-push-command.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Activate the virtual environment
+call "C:\Users\SynergexSystems\Documents\utilities\github-push-script\.venv\Scripts\activate.bat"
+
+REM Run the Python script
+python "C:\Users\SynergexSystems\Documents\utilities\github-push-script\git-push-command.bat"
+
+REM Deactivate the virtual environment
+deactivate
