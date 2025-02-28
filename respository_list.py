d2122d4709fbaf3d61af15e9d19db7b47df354bc
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-02-28 08:13:01 +0000

    Added testing, loical and remote repos.
    Changed to sets

--- respository_list.py
+++ respository_list.py
@@ -1,5 +1,17 @@
-def repository_list() -> list:
+def repository_list() -> set:
 
-    repositories = {"Scripts", "Experts", "Include", "Screenshots"}
+    repositories = {"Scripts", "Experts", "Include", "Images", "Files"}
 
     return repositories
+
+def repository_list_test() -> set:
+
+    repositories = {"Pictures"}
+
+    return repositories
+
+def remote_repositories() -> set:
+
+    remote_repositories = {"Experts", "Include", "Screenshots", "Scripts"}
+
+    return remote_repositories
