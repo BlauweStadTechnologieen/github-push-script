ad06158cef3baf2049b7040dbf42a8eff0e5188e
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-03-10 17:38:38 +0000

    Removed:
    SMTP Credentials,
    create_freshdesk_ticket module
    send_message_module
    email_body module
    generate_incident_ref
    aggign_log_number decorator
    send_message function calls

--- git-commit.py
+++ git-commit.py
@@ -10,18 +10,7 @@ import json as j
 from pathlib import Path
 from commit_notify import get_latest_commit
 import respository_list
-
-SMTP_SERVER     = "smtp-relay.brevo.com"
-SMTP_PORT       = 587
-SMTP_USER       = "448c41002@smtp-brevo.com"
-SMTP_PASSWORD   = "hSg19RUfw6QIcV7b"
-
-receiver_name   = "Synergex Systems"
-sender_name     =  "Blue City Capital Technologies Inc"
-
-sender_email    = "notifications@bluecitycapital.com"
-receiver_email  = "todd.gilbey@synergex-systems.com" 
-tech_department = "hello@bluecitycapital.com"
+import freshdesk_ticket
 
 directory_code  = "390295C323775C4285AE93D9818F5103"
 
@@ -33,168 +22,39 @@ LOGS_API_ENDPOINT_REGION    =   "uksouth"
 # Data Collection Endpoint
 DATA_COLLECTION_ENDPOINT    =   f"https://vmstatusdce-o0w0.{LOGS_API_ENDPOINT_REGION}-1.ingest.monitor.azure.com"
   
-def assign_log_number(func) -> str:
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        assign_log_number = generate_incident_ref()
-        try:
-            return func(*args, **kwargs, assign_log_number = assign_log_number)
-        except Exception as e:
-            raise e
-    return wrapper
+#def assign_log_number(func) -> str:
+#    @wraps(func)
+#    def wrapper(*args, **kwargs):
+#        assign_log_number = generate_incident_ref()
+#        try:
+#            return func(*args, **kwargs, assign_log_number = assign_log_number)
+#        except Exception as e:
+#            raise e
+#    return wrapper
 
-def create_freshdesk_ticket(logging_incident_number:str, exception_or_error_message:str, group_id:int = 201000039106, responder_id:int = 201002411183, subject:str = "Github is a true Git sometimes!") -> None:
-    """
-    Creates a Freshdesk ticket on behalf of the end user. This will be sent straight to the users inbox, where the user can add further information if they need to/
-    
-    This function must be called within a function which utulizes the assign_log_number decorator.
 
-    This function will only be called when an exception is thrown. The exception message will be passed to the 'exception' parameter.
-    """
-    
-    FRESHDESK_DOMAIN    = "bluecitycapitaltechnologies"
-    API_KEY             = "RTBtMGlwfVik2cuaj1"
-    API_URL             = f'https://{FRESHDESK_DOMAIN}.freshdesk.com/api/v2/tickets/'
-
-    description = f"This support ticket has been automatically generated because of the following error or exception message {exception_or_error_message}. Log number {logging_incident_number}"
-
-    ticket_data = {
-        "subject"     : subject,
-        "description" : description, 
-        'priority'    : 1,
-        'status'      : 2,
-        'group_id'    : group_id,
-        'responder_id': responder_id,
-        'requester'   : {
-            'name'    : receiver_name,
-            'email'   : receiver_email  
-        } 
-    }
-
-    custom_message  = None
-    ticket_id       = None
-    
-    try:
-        response = r.post(
-            API_URL,
-            auth    = (API_KEY, 'X'),
-            json    = j.dumps(ticket_data),
-            timeout = 30,
-            headers = {'Content-Type' : 'application/json'}
-        )
-
-    except TypeError as e:
-        custom_message = f"Type Error Exception: {e}"
-    
-    except r.RequestException as e:
-        custom_message = f"Requests Exception: {e}"
-
-    except Exception as e:
-        custom_message = f"General Exception: {e}"
-
-    else:
-    
-        if response.status_code == 201:
-            
-            ticket_info = response.json
-            ticket_id   = ticket_info.get("id")
-
-            print(ticket_id)
-
-        elif response.status_code == 429:
-            custom_message = f"API request limit exceeded: {response.status_code}"
-        
-        else:
-            custom_message = f"Support Ticket Creation Error. Error code: {response.status_code} Error HTTP response: {response.text} Error response {response.content}"
-
-    if custom_message:
-        print(custom_message)
-
-    return ticket_id
-
-def send_message(custom_message:str, sender_name:str, receiver_name:str, incident_ref:str) -> None | str:
-    """Sends a message which collects an incident number from the 'assign_log_number' decorator & passes it to the 'message_body' function."""
-    msg             = MIMEMultipart()
-    msg['Subject']  = "Github Push Incident"
-    msg['From']     = f'"{sender_name}" <{sender_email}>'
-    msg['To']       = receiver_email
-    body            = MIMEText(email_body(custom_message, sender_name, receiver_name, incident_ref), 'html')
-    msg.attach(body)
-    
-    try:
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()
-            server.login(SMTP_USER, SMTP_PASSWORD)
-            server.sendmail(sender_email, receiver_email, msg.as_string())
-    except Exception as e:
-        print(f"{e} {incident_ref}.")
-      
-    return
-    
-def email_body(custom_message:str, sender_name:str, receiver_name:str, incident_ref:str) -> str:
-    
-    was_ticket_generated    = create_freshdesk_ticket(incident_ref, custom_message)
-    ticket_id               = was_ticket_generated if was_ticket_generated is not None else "No support ticket available."
-    
-    ticket_return_message = (
-        "A support ticket was generated. You should receive this shortly."
-        if was_ticket_generated is not None
-        else "Apologies, but we encountered an internal technical issue that prevented the creation of a support ticket. You can still reach out to us by referencing the incident log number provided below. Thank you for your understanding and cooperation."
-    )
-
-    resource_data_table = f"""
-        <table border="0" cellpadding="5" cellspacing="0" style="border-collapse: collapse; text-align: left;">
-            <tr>
-                <th>Incident Number:</th>
-                <td>{incident_ref}</td>
-            </tr>
-            <tr>
-                <th>Support Ticket ID:</th>
-                <td>{ticket_id}</td>
-            </tr>
-            <tr>
-                <th>Degradation:</th>
-                <td>{custom_message}</td>
-            </tr>
-        </table>
-    """
-    return f"""Dear {receiver_name}<br><br>
-    We are writing to you because we have logged an incident pertaining to your Git Repo.<br><br>
-    {ticket_return_message}<br><br>
-    =========================<br><br>
-    {resource_data_table}<br>
-    =========================<br><br>
-    In the future, any error or incident will automatically generate a support ticket which will be sent - via email - to the users inbox where they will be abe to add any further information they may wish, be assigned a support engineer & to have a direct point of contact.<br><br>
-    Yours sincerely,<br><br>
-    {sender_name}
-    """
-
-def generate_incident_ref() -> str:
-    """Generates a universally unique identifier (UUID) to be used as a log number. UUID version 4 is used."""
-    return f"{uuid.uuid4()}"
-
-@assign_log_number
-def run_command(command:str, cwd:str = None, assign_log_number:str = None) -> str:
+def run_command(command:str, cwd:str = None) -> str:
     """Run a command in the terminal and capture the output. An email will be sent in the event of an error, stating the nature of the error. No message will be printed if there are no errors."""
     result = subprocess.run(command, cwd=cwd, text=True, capture_output=True)
     
     custom_message = None
+    custom_subject = "Run Command Error"
     
     if result.returncode != 0:
         custom_message =  result.stderr.strip() if result.stderr else "Unknown error occured - check repo directory as a possible solution"
-        send_message(custom_message, sender_name, receiver_name, assign_log_number)
+        freshdesk_ticket.create_freshdesk_ticket(custom_message, custom_subject)
         return custom_message
     else:
         print(f"Command Output: {result.stdout}") 
         return result.stdout
     
-@assign_log_number    
-def check_for_changes(cwd:str, assign_log_number:str = None) -> bool:
+def check_for_changes(cwd:str) -> bool:
     """Checks for both staged and unstaged changes in your local repo.
     """
     print("checking for changes....")
 
     custom_message = None
+    custom_subject = "An error occured when checking for changes in a local directory"
     
     try:
         result = run_command(["git", "status", "--short"], cwd).strip()
@@ -219,34 +79,39 @@ def check_for_changes(cwd:str, assign_log_number:str = None) -> bool:
         custom_message = f"General Exception {e}"
 
     if custom_message:
-        send_message(custom_message, sender_name, receiver_name, assign_log_number)
+        freshdesk_ticket.create_freshdesk_ticket(custom_message, custom_subject)
         print(custom_message)
 
     return False
 
-@assign_log_number 
-def is_valid_directory(cwd:str, assign_log_number:str = None) -> bool:
+def is_valid_directory(cwd:str) -> bool:
+    
+    custom_subject = "Invalid local directory"
+    
     if os.path.isdir(cwd):
         os.chdir(cwd)
         print(cwd)
-        if is_git_repo(cwd, assign_log_number):
+        if is_git_repo(cwd):
             return True
         else:
             return False
     else:
         custom_message = f"{cwd} is not a valid directory. Please check you have specified an existing directory & that this contains a .git folder."
-        print(custom_message)
-        send_message(custom_message, sender_name, receiver_name, assign_log_number)
+        print(custom_subject, custom_message)
+        freshdesk_ticket.create_freshdesk_ticket(custom_message,custom_subject)
         return False
 
 def is_git_repo(cwd, log_incident) -> bool:
-    git_path = os.path.join(cwd, '.git')
+    
+    git_path        = os.path.join(cwd, '.git')
+    custom_subject  = "Invalid Git Repo in the local directory"
+    
     if os.path.isdir(git_path):
         return True
     else:
         custom_message = f"{cwd} is not a Git Repository. Run 'git init' from the command shell "
-        print(custom_message)
-        send_message(custom_message,sender_name, receiver_name, log_incident)
+        print(custom_message, custom_subject)
+        freshdesk_ticket.create_freshdesk_ticket(custom_message, custom_subject)
         return False
     
 def push_to_github() -> None:
--- send_email.py
+++ send_email.py
@@ -1,31 +1,18 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+import email_auth
+import uuid
 
-DOMAIN              =   "@bluecitycapital.com"
-GITHUB_URL          =   "https://github.com/{owner}"
+def company_signoff():
+    return f"""
+    Yours sincerely<br>
+    <b>{email_auth.sender_name}</b><br>
+    {email_auth.sender_department} Team<br>
+    {email_auth.tech_department}<br><br>
+    """
 
-#SMTP Server Information
-SMTP_SERVER         =   "smtp-relay.brevo.com"
-SMTP_EMAIL          =   "448c41002@smtp-brevo.com"
-SMTP_PASSWORD       =   "hSg19RUfw6QIcV7b"
-SMTP_PORT           =   587
-
-#Recipient Information
-receiver_name       =   "Synergex Systems"
-receiver_email      =   "comms@synergex-systems.com"
-
-#Sender Information
-sender_name         =   "Blue City Capital Technologies, Inc"
-sender_email        =   f"notifications{DOMAIN}"
-sender_department   =   "Engineering"
-
-#Support Information
-tech_department     =   f"{sender_department.lower()}{DOMAIN}"
-
-def send_message(latest_commit_data:dict, changed_repo_list:list, github_owner:str, incident_ref:str = None, mime_type:str = "html") -> None:
+def send_message(latest_commit_data:dict, changed_repo_list:list, github_owner:str) -> None:
     
-    github_url = GITHUB_URL.format(owner=github_owner)
+    GITHUB_URL  =   "https://github.com/{owner}"
+    github_url  =   GITHUB_URL.format(owner=github_owner)
     
     resource_data_table = ""
     for data in latest_commit_data:
@@ -53,33 +40,47 @@ def send_message(latest_commit_data:dict, changed_repo_list:list, github_owner:s
                     <td>{data['id']}</td>
                 </tr>
             </table>
-        """
-    
+        """    
+            
     message_body = f"""
-        Dear {receiver_name}<br><br>
-        We are writing to you because you have a new commit uploaded to your GitHub repository.
-        Check it out by visiting your GitHub account at {github_url}<br><br>
-        {resource_data_table}
-        ========================================================================<br><br>
-        The local directories which have been changed since the last commit are {changed_repo_list}<br><br>
-        * You must be logged into the GitHub Repository in order to see the list of commits within the API call.<br><br>
-        Yours sincerely<br>
-        <b>{sender_name}</b><br>
-        {sender_department} Team<br>
-        {tech_department}<br><br>
+    Dear {email_auth.receiver_name}<br><br>
+    We are writing to you because you have a new commit uploaded to your GitHub repository.
+    Check it out by visiting your GitHub account at {github_url}<br><br>
+    {resource_data_table}
+    ========================================================================<br><br>
+    The local directories which have been changed since the last commit are {changed_repo_list}<br><br>
+    * You must be logged into the GitHub Repository in order to see the list of commits within the API call.<br><br>
+    {company_signoff()}
     """
+    email_auth.smtp_auth(message_body,"Github Commit Report")
+
+    return
+
+def generate_incident_uuid() -> str:
+    return f"""{uuid.uuid4()}"""
+
+def freshdesk_inop_notification(custom_message:str) -> None:
+      
+    incident_uuid   =   generate_incident_uuid()
+    custom_subject  =   "CRM Inoperable Notification"
     
-    msg             = MIMEMultipart()
-    msg['Subject']  = f"Github Commit Report"
-    msg['From']     = f'"{sender_name}" <{sender_email}>'
-    msg['To']       = receiver_email
-    body            = message_body
-    msg.attach(MIMEText(body, mime_type))
-    
-    try:
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()
-            server.login(SMTP_EMAIL, SMTP_PASSWORD)
-            server.sendmail(sender_email, receiver_email, msg.as_string())
-    except Exception as e:
-        print(f"Error sending email: {e}")
+    freshdesk_inop_text_body = f"""
+        Dear {email_auth.receiver_name}<br><br>
+        We are writing to you because our support ticketing system is inoperable
+        ========================================================================<br><br>
+        <table border="0" cellpadding="5" cellspacing="0" style="border-collapse: collapse; text-align: left;">
+            <tr>
+                <th>Freshdesk Inoperative Reason:</th>
+                <td>{custom_message}</td>
+            </tr>
+            <tr>
+                <th>Incident reference Number</th>
+                <td>{incident_uuid}</td>
+            </tr>
+        </table>
+        ========================================================================<br><br>
+        {company_signoff()}
+    """
+    email_auth.smtp_auth(freshdesk_inop_text_body, custom_subject)
+
+    return
