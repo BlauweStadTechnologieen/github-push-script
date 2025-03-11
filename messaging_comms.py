5d1c36813d6dd0231bd5bbc93de2f2663380c3f6
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-03-11 08:38:58 +0000

    Initial

---
+++ messaging_comms.py
@@ -0,0 +1,6 @@
+RECEIVER_DOMAIN     =   "@synergex-systems.com"
+SENDER_DOMAIN       =   "@bluecitycapital.us"
+receiver_name       =   "Synergex Systems Limited"
+receiver_email      =   f"comms{RECEIVER_DOMAIN}"
+sender_name         =   "Blue City Capital Technologies, Inc"
+sender_email        =   f"notifications{SENDER_DOMAIN}"
