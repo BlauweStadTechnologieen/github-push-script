f28225e39bf606a33a6e35c9ea629a7a0b111c03
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-03-11 08:37:56 +0000

    Implicitly import the freshdesk ticket module moved login credentials to new module

--- email_auth.py
+++ email_auth.py
@@ -1,10 +1,8 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import freshdesk_ticket
-
-RECEIVER_DOMAIN     =   "@synergex-systems.com"
-SENDER_DOMAIN       =   "@bluecitycapital.us"
+import messaging_comms
+from freshdesk_ticket import create_freshdesk_ticket
 
 #SMTP Authentication 
 SMTP_SERVER         =   "smtp-relay.brevo.com"
@@ -12,20 +10,13 @@ SMTP_EMAIL          =   "448c41002@smtp-brevo.com"
 SMTP_PASSWORD       =   "hSg19RUfw6QIcV7b"
 SMTP_PORT           =   587
 
-#User Information
-receiver_name       =   "Synergex Systems Limited"
-receiver_email      =   f"comms{RECEIVER_DOMAIN}"
-sender_name         =   "Blue City Capital Technologies, Inc"
-sender_email        =   f"notifications{SENDER_DOMAIN}"
-
 def smtp_auth(message_body:str, subject:str, mime_text:str = "html") -> bool:
     
-    custom_subject = "SMTP Authentication Error"
-    
+    custom_subject  = "SMTP Authentication Error"
     msg             = MIMEMultipart()
     msg['Subject']  = subject
-    msg['From']     = f'"{sender_name}" <{sender_email}>'
-    msg['To']       = receiver_email
+    msg['From']     = f'"{messaging_comms.sender_name}" <{messaging_comms.sender_email}>'
+    msg['To']       = messaging_comms.receiver_email
     body            = message_body
     msg.attach(MIMEText(body, mime_text))
     
@@ -33,12 +24,12 @@ def smtp_auth(message_body:str, subject:str, mime_text:str = "html") -> bool:
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
             server.starttls()
             server.login(SMTP_EMAIL, SMTP_PASSWORD)
-            server.sendmail(sender_email, receiver_email, msg.as_string())
+            server.sendmail(messaging_comms.sender_email, messaging_comms.receiver_email, msg.as_string())
             return True
     except Exception as e:
         custom_message = f"Error sending email: {e}"
         print(custom_message)
-        freshdesk_ticket.create_freshdesk_ticket(custom_message, custom_subject)
+        create_freshdesk_ticket(custom_message, custom_subject)
         return False
 
     return False
