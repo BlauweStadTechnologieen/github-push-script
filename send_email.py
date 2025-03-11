c790a9fa5d29f50d239127aca0bea1aeef4d95b9
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-03-11 08:40:04 +0000

    Implemented custom subject field. imported messaging_comms module Explicity imported the smtp_auth module

--- send_email.py
+++ send_email.py
@@ -1,18 +1,20 @@
-import email_auth
+import messaging_comms
+from email_auth import smtp_auth
 import uuid
 
-def company_signoff():
+def company_signoff() -> None:
     return f"""
     Yours sincerely<br>
-    <b>{email_auth.sender_name}</b><br>
-    {email_auth.sender_department} Team<br>
-    {email_auth.tech_department}<br><br>
+    <b>{messaging_comms.sender_name}</b><br>
+    {messaging_comms.sender_department} Team<br>
+    {messaging_comms.tech_department}<br><br>
     """
 
 def send_message(latest_commit_data:dict, changed_repo_list:list, github_owner:str) -> None:
     
-    GITHUB_URL  =   "https://github.com/{owner}"
-    github_url  =   GITHUB_URL.format(owner=github_owner)
+    GITHUB_URL      =   "https://github.com/{owner}"
+    github_url      =   GITHUB_URL.format(owner=github_owner)
+    custom_subject  =   "Github Commit Report"
     
     resource_data_table = ""
     for data in latest_commit_data:
@@ -43,7 +45,7 @@ def send_message(latest_commit_data:dict, changed_repo_list:list, github_owner:s
         """    
             
     message_body = f"""
-    Dear {email_auth.receiver_name}<br><br>
+    Dear {messaging_comms.receiver_name}<br><br>
     We are writing to you because you have a new commit uploaded to your GitHub repository.
     Check it out by visiting your GitHub account at {github_url}<br><br>
     {resource_data_table}
@@ -52,7 +54,7 @@ def send_message(latest_commit_data:dict, changed_repo_list:list, github_owner:s
     * You must be logged into the GitHub Repository in order to see the list of commits within the API call.<br><br>
     {company_signoff()}
     """
-    email_auth.smtp_auth(message_body,"Github Commit Report")
+    smtp_auth(message_body, custom_subject)
 
     return
 
@@ -65,7 +67,7 @@ def freshdesk_inop_notification(custom_message:str) -> None:
     custom_subject  =   "CRM Inoperable Notification"
     
     freshdesk_inop_text_body = f"""
-        Dear {email_auth.receiver_name}<br><br>
+        Dear {messaging_comms.receiver_name}<br><br>
         We are writing to you because our support ticketing system is inoperable
         ========================================================================<br><br>
         <table border="0" cellpadding="5" cellspacing="0" style="border-collapse: collapse; text-align: left;">
@@ -81,6 +83,6 @@ def freshdesk_inop_notification(custom_message:str) -> None:
         ========================================================================<br><br>
         {company_signoff()}
     """
-    email_auth.smtp_auth(freshdesk_inop_text_body, custom_subject)
+    smtp_auth(freshdesk_inop_text_body, custom_subject)
 
     return
