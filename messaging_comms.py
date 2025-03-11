e7ee0000885614435bdfdd03474a4652e765293e
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-03-11 10:48:41 +0000

    Moved the SENDER_DOMAIN over to email_auth and renamed to SMTP_DOMAIN as this is part of the SMTP authentication credentials

--- messaging_comms.py
+++ messaging_comms.py
@@ -1,8 +1,9 @@
+import email_auth
+
 RECEIVER_DOMAIN     =   "@synergex-systems.com"
-SENDER_DOMAIN       =   "@bluecitycapital.com"
 receiver_name       =   "Synergex Systems Limited"
 receiver_email      =   f"comms{RECEIVER_DOMAIN}"
 sender_name         =   "Blue City Capital Technologies, Inc"
-sender_email        =   f"notifications{SENDER_DOMAIN}"
+sender_email        =   f"notifications{email_auth.SMTP_DOMAIN}"
 sender_department   =   "Engineering"
-tech_department     =   f"{sender_department.lower()+SENDER_DOMAIN}"
+tech_department     =   f"{sender_department.lower()+email_auth.SMTP_DOMAIN}"
