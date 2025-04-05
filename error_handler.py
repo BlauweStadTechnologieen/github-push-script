1cb89dbac72de7a1f9860735452e20c3af9ef0e7
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-04-05 10:06:42 +0100

    Changed return Type tany in order to accommodate for non-bool functions

--- error_handler.py
+++ error_handler.py
@@ -1,6 +1,7 @@
 from freshdesk_ticket import create_freshdesk_ticket
 
-def report_error(subject:str, error_message:str) -> bool:
+def report_error(subject:str, error_message:str) -> None:
+    """Reports any error to the Freahdesk portal"""
     print(f"{subject} - { error_message}")
     #create_freshdesk_ticket(error_message, subject)
-    return False
+    return
