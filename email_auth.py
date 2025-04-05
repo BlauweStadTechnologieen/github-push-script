b4b4b1e9b4138584afffa19f94037854dbd54ab2
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-04-05 10:22:33 +0100

    Checks for missing SMTP credentials before proceeding to authentication.

--- email_auth.py
+++ email_auth.py
@@ -16,33 +16,52 @@ SMTP_CREDENTIALS = {
 }
 
 def smtp_auth(message_body:str, subject:str, mime_text:str = "html") -> bool:
-    
-    msg             = MIMEMultipart()
-    msg['Subject']  = subject
-    msg['From']     = f'"{settings_mapper.MESSAGING_METADATA["SENDER_NAME"]}" <{settings_mapper.MESSAGING_METADATA["SENDER_EMAIL"]}>'
-    msg['To']       = settings_mapper.MESSAGING_METADATA["REQUESTER_EMAIL"]
-    body            = message_body
-    msg.attach(MIMEText(body, mime_text))
-
+    """Authenticate the SMTP credentials, if False, it will terminate the programme"""
     try:
-        with smtplib.SMTP(SMTP_CREDENTIALS["SMTP_SERVER"], SMTP_CREDENTIALS["SMTP_PORT"]) as server:
-
-            server.starttls()
-
-            server.login(SMTP_CREDENTIALS["SMTP_EMAIL"], 
-                         SMTP_CREDENTIALS["SMTP_PASSWORD"]
-            )
-            server.sendmail(settings_mapper.MESSAGING_METADATA["SENDER_EMAIL"],
-                            settings_mapper.MESSAGING_METADATA["REQUESTER_EMAIL"], 
-                            msg.as_string()
-            )
-
-            return True
+        if not SMTP_CREDENTIALS["SMTP_EMAIL"] or not SMTP_CREDENTIALS["SMTP_PASSWORD"] or not SMTP_CREDENTIALS["SMTP_PORT"] or not SMTP_CREDENTIALS["SMTP_SERVER"]:
+            raise ValueError("One or nore of your SMTP credentials are mising, please chek these details.")
         
-    except Exception as e:
-
-        custom_message = f"Error sending email: {e}"
-        custom_subject  = "SMTP Authentication Error"
-
-        error_handler.report_error(custom_subject, custom_message)
+    except ValueError as e:
+        error_handler.report_error("Missing SMTO Credentials", f"{e}")
         return False
+    
+    except Exception as e:
+        error_handler.report_error("SMTO Authentication Error", f"{e}")
+        return False
+    
+    else:
+    
+        msg             = MIMEMultipart()
+        msg['Subject']  = subject
+        msg['From']     = f'"{settings_mapper.MESSAGING_METADATA["SENDER_NAME"]}" <{settings_mapper.MESSAGING_METADATA["SENDER_EMAIL"]}>'
+        msg['To']       = settings_mapper.MESSAGING_METADATA["REQUESTER_EMAIL"]
+        body            = message_body
+        msg.attach(MIMEText(body, mime_text))
+
+        try:
+            
+            with smtplib.SMTP(SMTP_CREDENTIALS["SMTP_SERVER"], SMTP_CREDENTIALS["SMTP_PORT"]) as server:
+
+                server.starttls()
+
+                server.login(SMTP_CREDENTIALS["SMTP_EMAIL"], 
+                            SMTP_CREDENTIALS["SMTP_PASSWORD"]
+                )
+                server.sendmail(settings_mapper.MESSAGING_METADATA["SENDER_EMAIL"],
+                                settings_mapper.MESSAGING_METADATA["REQUESTER_EMAIL"], 
+                                msg.as_string()
+                )
+
+                return True
+        
+        except ValueError as e:
+            error_handler.report_error("Missing SMTO Credentials", f"{e}")
+            return False
+        
+        except Exception as e:
+
+            custom_message = f"Error sending email: {e}"
+            custom_subject  = "SMTP Authentication Error"
+
+            error_handler.report_error(custom_subject, custom_message)
+            return False
