b5fd44aba8bd50d194042eee288c657a3b2cd5ec
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-04-07 15:32:06 +0100

    Adds descriptive commends

--- git-commit.py
+++ git-commit.py
@@ -106,11 +106,11 @@ def parent_directory_validation() -> str:
         parent_directory = settings_mapper.DIRECTORY_CONSTANTS["PARENT_DIRECTORY"]
 
         if not parent_directory:
-            #Checks to ensure that a parent directory is specified
+            # Checks to ensure that a parent directory is specified
             raise KeyError("PARENT_DIRECTORY key is missing from the DIRECTORY_CONSTANTS variables environment.")
         
         if not os.path.isdir(parent_directory):
-            #If a parent directory is specified, it will then check that this is valid.
+            # If a parent directory is specified, it will then check to ensure this is valid.
             raise ValueError(f"The specified parent directory in the PARENT_DIRECTORY key - {parent_directory} is invalid. Please verify the path and try again.")
             
     except KeyError as e:
