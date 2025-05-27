3446c01f6f9df426b3510c2ba311ad60b4827341
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-05-27 08:25:44 +0100

    Implemented debug checker - a support ticket will only be generated if debug is set to false.

--- error_handler.py
+++ error_handler.py
@@ -1,15 +1,20 @@
 from freshdesk_ticket import create_freshdesk_ticket
 
-def report_error(subject:str, error_message:str) -> None:
+def report_error(subject:str, error_message:str, debug:bool = False) -> None:
     """This method will be called whenever an error or Exception occurs. 
     A support ticket will be generated via the FreshDesk system. Please ensure that you regularly check your FreshDesk dashboard for any emergent issues. 
     Args:
-        subject(str): Denotes the subject of the error, which will be send to Freshdesk.
+        subject(str): Denotes the subject of the error, which will be sent to Freshdesk.
         error_message(str): Denoted the message and description of the error, which will be sent to Freshdesk.
-    Notes:
-    -
-        The Freshdesk system has not currently been rolled out, therefore, you will only see print statements in the command window. 
     """
     print(f"{subject} - { error_message}")
+
+    if debug:
+        
+        print("Debug mode is enabled. A support ticket will not be created whilst in debug mode.")
+
+        return
+    
     create_freshdesk_ticket(error_message, subject)
+
     return
