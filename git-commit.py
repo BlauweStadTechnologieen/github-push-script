2de7d237d4469dd6b3264af09bf0b292a763a1bc
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-06-26 13:43:38 +0100

    Remove add_gitignore_entries function and its calls from the git-commit script

--- git-commit.py
+++ git-commit.py
@@ -240,46 +240,6 @@ def is_valid_directory(cwd:str) -> bool:
     
     return True
 
-def add_gitignore_entries(cwd:str, name: str = ".gitignore") -> str | None:
-    """
-    Creates a .gitignore file with default entries if it does not already exist in the specified directory.
-    Parameters:
-        cwd (str): The directory in which to create or check for the .gitignore file.
-        name (str, optional): The name of the gitignore file. Defaults to ".gitignore".
-    Returns:
-        str | None: The path to the newly created .gitignore file if it was created, otherwise None.
-    Raises:
-        Reports any unexpected exceptions using the error_handler.
-    """
-
-    try:
-    
-        gitignore_path = os.path.join(cwd, name)
-
-        if not os.path.exists(gitignore_path):
-
-            with open(gitignore_path, "w") as write:
-
-                write.write("*\n")
-                write.write("!*.mq5\n")
-                write.write("!*.ex5\n")
-                write.write("!*.mqh\n")
-                write.write("!*.py\n")
- 
-                print(".gitignore file successfully added!")
-
-                return gitignore_path
-
-        else:
-
-            print(".gitignore file exists already.")
-
-            return None
-        
-    except Exception as e:
-        
-        error_handler.report_error("Unexpected error on writing .gitignore file", f"{e}")
-
 def is_git_repo(cwd:str) -> bool:
     
     """Once the current working directory is confirmed as valid, it will then check to ensure that the specified directory is a valid git repository.
@@ -396,13 +356,11 @@ def push_to_github() -> None:
                 continue
 
             if not is_git_repo(cwd):
-
+                
                 init_git_pull_command(cwd, remote_repo, github_username)
                 
                 continue
-
-            add_gitignore_entries(cwd)
-                                            
+                 
             if not run_me_them_commands(cwd, remote_repo):
                 
                 continue
