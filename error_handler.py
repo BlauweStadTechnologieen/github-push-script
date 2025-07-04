4fc5619f5defa6fc412922c7ce6ba7b51ee3acf6
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-07-04 13:46:11 +0100

    Refactor report_error function: streamline logging configuration and remove email reporting

--- error_handler.py
+++ error_handler.py
@@ -1,29 +1,32 @@
 from freshdesk_ticket import create_freshdesk_ticket
-from send_email import send_error_report_by_email
 import logging
+import sys
 
-logging.basicConfig(filename="logger.log", filemode='a', format='%(asctime)s - %(funcName)s')
+# DON'T disable logging unless absolutely necessary
+# logging.disable(logging.CRITICAL)
 
-logging.disable(logging.CRITICAL)
-logging.disable()
+def report_error(subject: str, error_message: str, logging_level=logging.DEBUG) -> None:
 
-def report_error(subject:str, error_message:str, debug:bool = False) -> None:
-    """This method will be called whenever an error or Exception occurs. 
-    A support ticket will be generated via the FreshDesk system. Please ensure that you regularly check your FreshDesk dashboard for any emergent issues. 
-    Args:
-        subject(str): Denotes the subject of the error, which will be sent to Freshdesk.
-        error_message(str): Denoted the message and description of the error, which will be sent to Freshdesk.
-    """
-    print(f"{subject} - { error_message}")
+    # Reset logging handlers
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
 
-    if debug:
-        
-        print("Debug mode is enabled. A support ticket will not be created whilst in debug mode.")
+    # Configure logging
+    logging.basicConfig(
+
+        level=logging_level,
+        format='%(levelname)s: %(message)s: %(funcName)s'
+
+    )
+
+    if logging_level <= logging.WARNING:
+
+        print(f"{subject} - {error_message} - {logging_level}")
 
         return
-    
-    send_error_report_by_email(subject, error_message)
-    
-    #create_freshdesk_ticket(error_message, subject)
 
-    return
+    if logging_level >= logging.ERROR:
+
+        create_freshdesk_ticket(error_message, subject)
+
+        sys.exit(f"Exiting due to incident: " + subject)
