1004541d258598bc84f0de569c1ede4743e98e38
BlauweStadTech <engineering@bluecitycapital.com>
2025-11-03 13:44:41 +0000

    Add success logging functionality to log successful events

---
+++ message.py
@@ -0,0 +1,11 @@
+import logging
+
+logging.basicConfig(filename="success.log", filemode='a',
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
+
+def success_log(success_subject: str, success_message: str) -> None:
+    
+    logging.debug(f"{success_subject} - {success_message}")
+
+    return
